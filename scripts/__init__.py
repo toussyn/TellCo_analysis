eac4d338add8eca60157d20a341a7e83fbf452aa
MACBOOK <macbook@hatice.local>
2022-08-24 05:50:32 +0100

    handling missing valuees

---
+++ scripts/__init__.py
@@ -0,0 +1,1 @@
+from . import dataframe_cleaner
--- scripts/dataframe_cleaner.py
+++ scripts/dataframe_cleaner.py
@@ -1,24 +1,24 @@
 import numpy as np
 import pandas as pd
 
-class CleanerDF():
-    def percent_missing(self, datf):
+def percent_missing(self, datf):
 
-        # Calculate total number of cells in dataframe
-        total_cells = np.product(datf.shape)
+    # Calculate total number of cells in dataframe
+    total_cells = np.product(datf.shape)
 
-        # Count number of missing values per column
-        total_missing = datf.isnull().sum().sum()
+    # Count number of missing values per column
+    total_missing = datf.isnull().sum().sum()
 
-        # Calculate percentage of missing values
-        return ("The Diabetes dataset contains", round(((total_missing/total_cells) * 100), 2), "%", "missing values.")
+    # Calculate percentage of missing values
+    return ("The Diabetes dataset contains", round(((total_missing/total_cells) * 100), 2), "%", "missing values.")
 
-    def percent_missing_values(self, datf):
-        #Get Missing value percentage of columns
-        percent_missing = datf.isnull().sum() * 100 / len(df)
-        missing_value_df = pd.DataFrame({'column_name': datf.columns,
+def percent_missing_values(self, datf):
+        
+    #Get Missing value percentage of columns
+    percent_missing = datf.isnull().sum() * 100 / len(df)
+    missing_value_df = pd.DataFrame({'column_name': datf.columns,
                                          'percent_missing': percent_missing})
 
-        missing_value_df.sort_values('percent_missing', inplace=True)
+    missing_value_df.sort_values('percent_missing', inplace=True)
         
-        return missing_value_df
+    return missing_value_df
